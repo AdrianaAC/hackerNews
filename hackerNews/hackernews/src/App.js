ad98abda282cc230f8ef82edfa93fc4cd642c267
Adriana <adrianaalves098@gmail.com>
2021-02-16 09:05:32 +0000

    page37

--- hackerNews/hackernews/src/App.js
+++ hackerNews/hackernews/src/App.js
@@ -21,22 +21,40 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    // const isNotId = (item) => item.objectID !== id;
+    // const updatedList = this.state.filter(isNotId);
+
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
 
   render() {
     return (
       <div className="App">
-        {list.map((item) => 
-            <div key={item.objectID}>
-              <ul>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <li>{item.author}</li>
-                <li>{item.num_comments}</li>
-                <li>{item.points}</li>
-              </ul>
-            </div>
-          )}
+        {this.state.list.map((item) => (
+          <div key={item.objectID}>
+            <ul>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <li>{item.author}</li>
+              <li>{item.num_comments}</li>
+              <li>{item.points}</li>
+            </ul>
+            <button onClick={() => this.onDismiss(item.objectID)}>
+              Dismiss
+            </button>
+          </div>
+        ))}
       </div>
     );
   }
